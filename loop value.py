31b5a7b29f349bb07b31af2bac620b78680c1c53
jenifer1818 <arulchristober1818@gmail.com>
2024-06-21 10:40:33 +0530

    Add files via upload

---
+++ loop value.py
@@ -0,0 +1,100 @@
+#loop value
+
+for i in range(2,9):
+    print (i)
+    
+for g in range(0,10,2):
+    print(g)
+    
+for j in range(0,10,3):
+    print (j)
+for k in range(0,1000,100):
+    print(k)
+for e in range(-10,-1):
+    print(e)
+    
+#while loop
+i=10
+while i<100:
+    print(i)
+    i+=5
+print("name")
+x="jeni"
+i=0
+while i<(len(x)):
+    print(x[i],end=" ")
+    i+=1
+
+
+    
+#brake
+    
+'''for i in range(12):
+    print(i)
+    if i==5:
+        break
+i=0    
+while i<12:
+    print(i)
+    i+=1
+    if i==5:    
+        break
+
+x='jeni'
+for i in x:
+     if i=='n':
+        break
+    print(i)'''
+    
+#continiue
+'''for i in range(11):
+    print (i)
+    if i==1:
+        continue
+i=0
+while i<10:
+    i+=1
+    if i==6:
+        continue
+    print(i)
+x='jeni'
+for i in x:
+    if i=='i':
+        
+        continue
+    print(i)
+
+#pass
+x='jeni'
+for i in x:
+    if i=='n':
+        pass
+    print(i)
+i=0
+while i<10:
+    i+=1
+    if i==5:
+        pass
+    print(i)'''
+
+
+#name method ex=level
+x='level'    
+for i in x:
+    if i =='e':
+        break
+    print(i)
+
+
+
+    
+    
+    
+    
+
+    
+    
+    
+
+    
+    
