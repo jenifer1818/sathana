6c142c7469c7314b4d1aa197164ad33a1182936e
jeni <arulchristober1818@gmail.com>
2024-06-21 12:28:13 +0530

    added and deleted some files

---
+++ group.py
@@ -0,0 +1,133 @@
+#for long,
+'''for j in range (-10,0,2):
+    print(j)
+    
+ 
+x=[1,2,3,4,5,6,7,8]
+for g in (x):
+    print(g)
+print(g)
+    
+x=['jeni']
+for i in (x):
+    print(i)
+print(i)'''
+
+
+#factorial
+'''x=int(input('enter the value'))
+fact=1
+for g in range(1,x+1):
+    fact=fact*g
+
+print(fact)
+
+
+#factorial
+y=int(input('enter the value'))
+fact=1
+for g in range(1,y+1):
+    fact=fact*g
+print(fact)'''
+
+
+#even no
+
+'''num=int(input("enter number"))
+if(num%2)==0:
+    print("enter the number even")
+else:
+    print("enter the number odd")'''
+
+#for range
+'''for j in range(0,20+1,2):
+    print (j)
+for x in range(-1,-20+1,-2):
+    print(x)'''
+
+    
+#factorial
+
+'''n=int(input('enter no'))
+fact=1
+if n>=1:
+    for i in range(1,n+1):
+        fact=fact*i
+        print(f'fact of{n}is{fact}')'''
+
+#string method
+
+x=0
+y=0
+z=0
+a=0
+x='PyThOn LaNgUaGe'
+for i in x:
+    if (i.isupper()):
+        y+=i.count(i)
+        print('count of upper case letters',y)
+        
+x='jenifer123'
+for i in x:
+    if(i.isnumeric()):
+        z+=i.count(i)
+    print ('count of numeric numbers',z)
+    
+
+
+        
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+      
+            
+
